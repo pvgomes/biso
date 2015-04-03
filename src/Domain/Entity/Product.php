e7f0faa223042882558b31cde8205d5ad5c2e037
pvgomes <paulo.lima@tricae.com.br>
2015-04-03 08:29:09 -0300

    First catalog domain structure by Class Diagram

---
+++ src/Domain/Entity/Product.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Domain\Entity;
+
+use Domain\VO;
+
+class Product
+{
+
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var string
+     */
+    private $ean;
+
+    /**
+     * @var int
+     */
+    private $quantity;
+
+    /**
+     * @var VO\Price
+     */
+    private $price;
+
+    /**
+     * @var VO\Measure
+     */
+    private $measure;
+
+    /**
+     * @var Category
+     */
+    private $category;
+
+    /**
+     * @var VO\Specification
+     */
+    private $specification;
+
+    public function __construct(Category $category,
+                                VO\Specification $specification,
+                                VO\Price $price,
+                                VO\Measure $measure,
+                                $id, $quantity)
+    {
+        $this->category = $category;
+        $this->id = $id;
+        $this->measure = $measure;
+        $this->price = $price;
+        $this->specification = $specification;
+        $this->quantity = $quantity;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @param string $ean
+     */
+    public function setEan($ean)
+    {
+        $this->ean = $ean;
+    }
+
+    /**
+     * @return string
+     */
+    public function ean()
+    {
+        return $this->ean;
+    }
+
+    /**
+     * @return string
+     */
+    public function name()
+    {
+        return $this->name();
+    }
+
+    /**
+     * @return string
+     */
+    public function description()
+    {
+        return $this->description;
+    }
+
+    public function addQuantity($quantity)
+    {
+        $this->quantity = (int) ($this->quantity + $quantity);
+    }
+
+    public function removeQuantity($quantity)
+    {
+        $this->quantity = (int) ($this->quantity - $quantity);
+    }
+
+    public function isAvailable()
+    {
+        $isAvailable = true;
+
+        if ($this->quantity <= 0 || !$this->isEanValid()) {
+            $isAvailable = false;
+        }
+
+        return $isAvailable;
+    }
+
+    private function isEanValid()
+    {
+        $isEanValid = true;
+
+        if (strlen($this->ean) <= 10 || is_numeric($this->ean)) {
+            $isEanValid = false;
+        }
+
+        return $isEanValid;
+    }
+}
---
+++ src/Domain/VO/Specification.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Domain\VO;
+
+class Specification {
+
+    private $size;
+    private $width;
+    private $weight;
+
+    public function __construct($size, $width, $weight)
+    {
+        $this->size = $size;
+        $this->width = $width;
+        $this->weight = $weight;
+    }
+
+    /**
+     * @return string
+     */
+    public function size()
+    {
+        return $this->size();
+    }
+
+    /**
+     * @return string
+     */
+    public function width()
+    {
+        return $this->width;
+    }
+
+    /**
+     * @return string
+     */
+    public function weight()
+    {
+        return $this->weight;
+    }
+
+} 
