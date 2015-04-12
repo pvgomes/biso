dfe011f64f01403b0b320affde1417a2a12b9b86
pvgomes <paulo.lima@tricae.com.br>
2015-04-12 15:36:07 -0300

    Product build

--- src/Domain/Factory/Product.php
+++ src/Domain/Factory/Product.php
@@ -7,14 +7,14 @@ use Domain\VO;
 
 class Product {
 
-    public static function build(array $data)
+    public static function build(\stdClass $data)
     {
-        $specification = new VO\Specification($data['size'], $data['width'], $data['weight']);
-        $originalMoney = new VO\Money($data['originalPrice'], new VO\Currency($data['currency']));
-        $specialMoney = new VO\Money($data['specialPrice'], new VO\Currency($data['currency']));
+        $specification = new VO\Specification($data->size, $data->width, $data->weight);
+        $originalMoney = new VO\Money($data->originalPrice, new VO\Currency($data->currency));
+        $specialMoney = new VO\Money($data->specialPrice, new VO\Currency($data->currency));
         $price = new VO\Price($originalMoney, $specialMoney);
 
-        return new Entity\Product($specification, $price, $data['id'], $data['quantity']);
+        return new Entity\Product($specification, $price, $data->id, $data->quantity);
     }
 
 }
