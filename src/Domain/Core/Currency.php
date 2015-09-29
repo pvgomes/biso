c9dd83f81f3261c7ed8e3fafaad3ac349a9ce371
Paulo Victor Gomes <paulo.lima@tricae.com.br>
2015-09-29 09:45:56 -0300

    simple vos

---
+++ src/Domain/Core/Currency.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Domain\Core;
+
+class Currency
+{
+
+    Const REAL = "BRL";
+    Const DOLAR = "USD";
+
+    private $isoCode;
+
+    public function __construct($anIsoCode = self::REAL)
+    {
+        $this->setIsoCode($anIsoCode);
+    }
+    private function setIsoCode($anIsoCode)
+    {
+        if (!preg_match('/^[A-Z]{3}$/', $anIsoCode)) {
+            throw new \InvalidArgumentException();
+        }
+        $this->isoCode = $anIsoCode;
+    }
+
+    public function isoCode()
+    {
+        return $this->isoCode;
+    }
+
+    public function equals(Currency $currency)
+    {
+        return $currency->isoCode() === $this->isoCode();
+    }
+
+}
---
+++ src/Domain/Core/Money.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Domain\Core;
+
+class Money
+{
+
+    private $amount;
+    private $currency;
+
+    public function __construct($anAmount, Currency $aCurrency)
+    {
+        $this->setAmount($anAmount);
+        $this->setCurrency($aCurrency);
+    }
+
+    private function setAmount($anAmount)
+    {
+        $this->amount = (int)$anAmount;
+    }
+
+    private function setCurrency(Currency $aCurrency)
+    {
+        $this->currency = $aCurrency;
+    }
+
+    public function amount()
+    {
+        return $this->amount;
+    }
+
+    public function currency()
+    {
+        return $this->currency;
+    }
+
+    public function add(Money $money)
+    {
+        if (!$money->currency()->equals($this->currency())) {
+            throw new \InvalidArgumentException();
+        }
+        return new self(
+            $money->amount() + $this->amount(),
+            $this->currency()
+        );
+    }
+
+    public static function fromMoney(Money $aMoney)
+    {
+        return new self(
+            $aMoney->amount(),
+            $aMoney->currency()
+        );
+    }
+
+    public static function ofCurrency(Currency $aCurrency)
+    {
+        return new self(0, $aCurrency);
+    }
+
+    public function equals(Money $money)
+    {
+        return
+            $money->currency()->equals($this->currency()) &&
+            $money->amount() === $this->amount();
+    }
+
+}
